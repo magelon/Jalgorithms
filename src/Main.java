9b8c8e388507d216c5bc29b7a2798b70e099f952
magelon <alfardproject@gmail.com>
2022-05-09 16:18:13 -0700

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String obj="String object";
+        System.out.println("Hello world!"+obj.substring(1));
     }
 }
